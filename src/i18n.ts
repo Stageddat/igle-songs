e139175521911d289f5225f82484f061003a84d4
stageddat <chmarc72@gmail.com>
2025-06-25 19:12:47 +0200

    refact: remove useless comments

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 // i18n.ts
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector"; // Import LanguageDetector
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "../public/locales/en/translation.json";
 import es from "../public/locales/es/translation.json";
