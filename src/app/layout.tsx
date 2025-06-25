60df444e58ccfee04ac75504eed7e0deb797abd9
stageddat <chmarc72@gmail.com>
2025-06-25 19:20:30 +0200

    feat: enhance language management in navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
-// app/layout.tsx
 "use client";
 
 import React from "react";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { useTranslation } from "react-i18next";
-import "@/i18n"; // Import your i18n configuration here to ensure it's initialized
+import "@/i18n";
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	// const { i18n: i18nextInstance } = useTranslation();
-
-	// const primaryLang = i18nextInstance.language;
 	const { t } = useTranslation();
 
 	return (
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
 	NavigationMenu,
 	NavigationMenuLink,
@@ -11,6 +11,11 @@ import i18n from "@/i18n";
 export default function Navbar() {
 	const { t } = useTranslation();
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const [currentLang, setCurrentLang] = useState("");
+
+	useEffect(() => {
+		setCurrentLang(i18n.language);
+	}, []);
 
 	const languages = [
 		{ code: "es", name: "Español" },
@@ -21,10 +26,8 @@ export default function Navbar() {
 
 	const handleLanguageChange = (newLocale: string) => {
 		i18n.changeLanguage(newLocale);
-		if (typeof window !== 'undefined') {
-			localStorage.setItem("i18nextLng", newLocale);
-		}
-
+		localStorage.setItem("i18nextLng", newLocale);
+		setCurrentLang(newLocale);
 		setIsDropdownOpen(false);
 	};
 
@@ -54,8 +57,8 @@ export default function Navbar() {
 						onClick={() => setIsDropdownOpen(!isDropdownOpen)}
 						className="px-4 py-2 bg-sidebar-foreground text-sidebar rounded-lg shadow hover:bg-sidebar-foreground/90 transition-colors duration-200"
 					>
-						{languages.find((lang) => lang.code === i18n.language)?.name ||
-							i18n.language.toUpperCase()}
+						{languages.find((lang) => lang.code === currentLang)?.name ||
+							currentLang?.toUpperCase()}
 					</button>
 					{isDropdownOpen && (
 						<div className="absolute right-0 mt-2 w-40 bg-card border border-border rounded-lg shadow-lg z-10">
@@ -63,7 +66,7 @@ export default function Navbar() {
 								<button
 									key={lang.code}
 									onClick={() => handleLanguageChange(lang.code)}
-									className={`block w-full text-left px-4 py-2 hover:bg-accent hover:text-accent-foreground rounded-lg transition-colors duration-200 ${i18n.language === lang.code
+									className={`block w-full text-left px-4 py-2 hover:bg-accent hover:text-accent-foreground rounded-lg transition-colors duration-200 ${currentLang === lang.code
 										? "bg-accent text-accent-foreground"
 										: "text-foreground"
 										}`}
