14b09a759fee5cc28abf39f9ce0cffcd9b346751
stageddat <chmarc72@gmail.com>
2025-07-06 14:30:44 +0200

    feat: support 1 song file

--- src/modules/fileManager/filterSongs.ts
+++ src/modules/fileManager/filterSongs.ts
@@ -1,22 +1,40 @@
-export function filterSongs(songName: string): [string, string] | false {
-  const regex = /^([^_]+)_([^_]+)\.pptx$/i;
-  const match = songName.match(regex);
+export function filterSongs(
+  songName: string
+): [string] | [string, string] | false {
+  const twoParts = /^([^_]+)_([^_]+)\.pptx$/i;
+  const onePart = /^([^_]+)\.pptx$/i;
 
+  let match = songName.match(twoParts);
   if (match) {
     const [, part1, part2] = match;
     return [part1, part2];
   }
 
+  match = songName.match(onePart);
+  if (match) {
+    const [, part1] = match;
+    return [part1];
+  }
+
   return false;
 }
 
-export function filterSongNames(songName: string): [string, string] | false {
-  const regex = /^([^_]+)_([^_]+)$/i;
-  const match = songName.match(regex);
+export function filterSongNames(
+  songName: string
+): [string] | [string, string] | false {
+  const twoParts = /^([^_]+)_([^_]+)$/i;
+  const onePart = /^([^_]+)$/i;
 
+  let match = songName.match(twoParts);
   if (match) {
     const [, part1, part2] = match;
     return [part1, part2];
+  }
+
+  match = songName.match(onePart);
+  if (match) {
+    const [, part1] = match;
+    return [part1];
   }
 
   return false;
