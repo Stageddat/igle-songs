d72176a59fe0f857c6bed2dddafb62b6ebe59382
Stageddat <chmarc72@gmail.com>
2026-01-11 21:29:40 +0100

    feat: change default locale to chinese

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ["es", "en", "ca", "ch"] as const;
-export const defaultLocale: Locale = "es";
+export const defaultLocale: Locale = "ch";
